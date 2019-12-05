6cf98ee5c59686443d0d4b011a61b61d23e1c71c
austi <austin.green@outlook.com>
2019-12-04 20:07:42 -0800

    Switch to cuda project

---
+++ LaneDetection/main.cu
@@ -0,0 +1,15 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	cv::Mat img = cv::imread("test.png");
+	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	imshow("image", img);
+	cv::waitKey(0);
+	return 0;
+}
