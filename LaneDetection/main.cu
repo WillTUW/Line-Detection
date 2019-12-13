3b091d3074aaf5925e6dff541c967e17fbfe8af1
austi <austin.green@outlook.com>
2019-12-12 21:43:57 -0800

    Split into CPU and GPU versions

--- LaneDetection/main.cu
+++ LaneDetection/main.cu
@@ -112,7 +112,7 @@ void UpdateAccumulatorAll(int count, int *queueX, int *queueY, int numrho, short
 	cudaFree(dev_max_n);
 }
 
-void HoughLinesProbabilistic_v2(cv::Mat& image, int threshold, int lineLength, int lineGap, std::vector<cv::Vec4i>& lines, int linesMax)
+void HoughLines_GPU(cv::Mat& image, int threshold, int lineLength, int lineGap, std::vector<cv::Vec4i>& lines, int linesMax)
 {
 	cv::Point pt;
 	cv::RNG rng((uint64)-1);
@@ -349,6 +349,216 @@ void HoughLinesProbabilistic_v2(cv::Mat& image, int threshold, int lineLength, i
 	}
 }
 
+void HoughLines_CPU(cv::Mat& image, int threshold, int lineLength, int lineGap, std::vector<cv::Vec4i>& lines, int linesMax)
+{
+	cv::Point pt;
+	cv::RNG rng((uint64)-1);
+
+	CV_Assert(image.type() == CV_8UC1);
+
+	int width = image.cols;
+	int height = image.rows;
+
+	int numrho = cvRound(((width + height) * 2 + 1) / RHO);
+
+	cv::Mat accum = cv::Mat::zeros(NUM_ANGLE, numrho, CV_16SC1);
+	cv::Mat mask(height, width, CV_8UC1);
+
+	uchar* mdata0 = mask.ptr();
+	std::vector<int> nzlocX, nzlocY;
+
+	// stage 1. collect non-zero image points
+	for (pt.y = 0; pt.y < height; pt.y++)
+	{
+		const uchar* data = image.ptr(pt.y);
+		uchar* mdata = mask.ptr(pt.y);
+		for (pt.x = 0; pt.x < width; pt.x++)
+		{
+			if (data[pt.x])
+			{
+				mdata[pt.x] = (uchar)1;
+				nzlocX.push_back(pt.x);
+				nzlocY.push_back(pt.y);
+			}
+			else
+			{
+				mdata[pt.x] = 0;
+			}
+		}
+	}
+
+	int count = (int)nzlocX.size();
+	short* adata = accum.ptr<short>();
+
+	// stage 2. process all the points in random order
+	for (; count > 0; count--)
+	{
+		// choose random point out of the remaining ones
+		int idx = rng.uniform(0, count);
+
+		cv::Point point = cv::Point(nzlocX[idx], nzlocY[idx]);
+		cv::Point line_end[2];
+
+		int i = point.y, j = point.x;
+		int k, dx0, dy0;
+		bool xflag, good_line;
+
+		// "remove" it by overriding it with the last element
+		nzlocX[idx] = nzlocX[count - 1];
+		nzlocY[idx] = nzlocY[count - 1];
+
+		// check if it has been excluded already (i.e. belongs to some other line)
+		if (!mdata0[i*width + j])
+			continue;
+
+		int max_n = 0;
+		int max_val = threshold - 1;
+
+		// update accumulator, find the most probable line
+		for (int n = 0; n < NUM_ANGLE; n++)
+		{
+			int r = cvRound(j * hough_cos(n) + i * hough_sin(n));
+			r += (numrho - 1) / 2;
+			int val = ++adata[r + (n * numrho)];
+			if (val > max_val)
+			{
+				max_val = val;
+				max_n = n;
+			}
+		}
+
+		if (max_val < threshold)
+			continue;
+
+		// from the current point walk in each direction
+		// along the found line and extract the line segment
+		float a = -sin(max_n * M_THETA) * IRHO;
+		float b = cos(max_n * M_THETA) * IRHO;
+		int x0 = j;
+		int y0 = i;
+		if (fabs(a) > fabs(b))
+		{
+			xflag = true;
+			dx0 = a > 0 ? 1 : -1;
+			dy0 = cvRound(b*(1 << SHIFT) / fabs(a));
+			y0 = (y0 << SHIFT) + (1 << (SHIFT - 1));
+		}
+		else
+		{
+			xflag = false;
+			dy0 = b > 0 ? 1 : -1;
+			dx0 = cvRound(a*(1 << SHIFT) / fabs(b));
+			x0 = (x0 << SHIFT) + (1 << (SHIFT - 1));
+		}
+
+		for (k = 0; k < 2; k++)
+		{
+			int gap = 0, x = x0, y = y0, dx = dx0, dy = dy0;
+
+			if (k > 0)
+				dx = -dx, dy = -dy;
+
+			// walk along the line using fixed-point arithmetic,
+			// stop at the image border or in case of too big gap
+			for (;; x += dx, y += dy)
+			{
+				int i1, j1;
+
+				if (xflag)
+				{
+					j1 = x;
+					i1 = y >> SHIFT;
+				}
+				else
+				{
+					j1 = x >> SHIFT;
+					i1 = y;
+				}
+
+				if (j1 < 0 || j1 >= width || i1 < 0 || i1 >= height)
+					break;
+
+				uchar* mdata = mdata0 + i1 * width + j1;
+
+				// for each non-zero point:
+				//    update line end,
+				//    clear the mask element
+				//    reset the gap
+				if (*mdata)
+				{
+					gap = 0;
+					line_end[k].y = i1;
+					line_end[k].x = j1;
+				}
+				else if (++gap > lineGap)
+					break;
+			}
+		}
+
+		good_line = std::abs(line_end[1].x - line_end[0].x) >= lineLength ||
+			std::abs(line_end[1].y - line_end[0].y) >= lineLength;
+
+		for (k = 0; k < 2; k++)
+		{
+			int x = x0, y = y0, dx = dx0, dy = dy0;
+			if (k > 0)
+			{
+				dx = -dx, dy = -dy;
+			}
+
+			// walk along the line using fixed-point arithmetic,
+			// stop at the image border or in case of too big gap
+			for (;; x += dx, y += dy)
+			{
+
+				int i1, j1;
+
+				if (xflag)
+				{
+					j1 = x;
+					i1 = y >> SHIFT;
+				}
+				else
+				{
+					j1 = x >> SHIFT;
+					i1 = y;
+				}
+
+				uchar* mdata = mdata0 + i1 * width + j1;
+
+				// for each non-zero point:
+				//    update line end,
+				//    clear the mask element
+				//    reset the gap
+				if (*mdata)
+				{
+					if (good_line)
+					{
+						for (int n = 0; n < NUM_ANGLE; n++)
+						{
+							int r = cvRound(j1 * hough_cos(n) + i1 * hough_sin(n));
+							r += (numrho - 1) / 2;
+							adata[r + (n * numrho)]--;
+						}
+					}
+					*mdata = 0;
+				}
+
+				if (i1 == line_end[k].y && j1 == line_end[k].x)
+					break;
+			}
+
+			if (good_line)
+			{
+				cv::Vec4i lr(line_end[0].x, line_end[0].y, line_end[1].x, line_end[1].y);
+				lines.push_back(lr);
+				if ((int)lines.size() >= linesMax)
+					return;
+			}
+		}
+	}
+}
+
 int main()
 {
 	const int ddepth = CV_16S;
@@ -382,7 +592,7 @@ int main()
 	auto start = std::chrono::high_resolution_clock::now();
 
 	std::vector<cv::Vec4i> lines;
-	HoughLinesProbabilistic_v2(canny, 80, 200, 10, lines, 10);
+	HoughLines_GPU(canny, 80, 200, 10, lines, 10);
 
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double, std::ratio<1, 1000>> time = end - start;
